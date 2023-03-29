c223b3f63d10fff01186c8d4560435f85e10eaee
Gowroji Sunil <48122892+sgowroji@users.noreply.github.com>
2023-03-29 12:20:04 +0530

    Update issue_priority.js

--- .github/workflows/scripts/issue_priority.js
+++ .github/workflows/scripts/issue_priority.js
@@ -54,3 +54,54 @@ module.exports = async ({ github, context }) => {
         }
     }
 }
+module.exports = async ({ github, context }) => {
+   
+    //fetch all the open issues with label P1
+    let issues = await github.rest.issues.listForRepo({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        state: "open",
+        labels: "P1"
+    });
+    if (issues.status != 200)
+        return
+   let issueList = issues.data
+    for (let i = 0; i < issueList.length; i++) {
+        let number = issueList[i].number;
+        // fetch label all the events inside issues 
+        let resp = await github.rest.issues.listEventsForTimeline({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: number,
+        });
+        let events = resp.data;
+        for (let i = 0; i < events.length; i++) {
+            let event_details = events[i];
+            console.log("event_details",event_details)
+            if (event_details.event == 'labeled' && event_details.label && event_details.label.name == "P1") {
+                let currentDate = new Date();
+                let labeledDate = new Date(event_details.created_at)
+                console.log("time diff",currentDate - labeledDate)
+                if (currentDate - labeledDate > 2) {
+                    //remove label P1 if more then 60 days old
+                    await github.rest.issues.removeLabel({
+                        issue_number: number,
+                        owner: context.repo.owner,
+                        repo: context.repo.repo,
+                        name: "P1"
+
+                    })
+                    //add label P2
+                    await github.rest.issues.addLabels({
+                        issue_number: number,
+                        owner: context.repo.owner,
+                        repo: context.repo.repo,
+                        labels:["P2"]
+
+                    })
+                }
+
+            }
+        }
+    }
+}
